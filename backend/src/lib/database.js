3ef408dcd89c30f8e767631eafb1106f3e3f6582
Ahmadalmuhidat <ahmad.almuhidat@gmail.com>
2026-01-14 13:20:40 +0300

    validate database url

--- backend/src/lib/database.js
+++ backend/src/lib/database.js
@@ -3,6 +3,10 @@ import { ENV } from './env.js';
 
 export const connectToDatabase = async () => {
   try {
+    if (!ENV.MONGODB_URI) {
+      throw new Error('MONGODB_URI is not defined in environment variables');
+    }
+
     await mongoose.connect(ENV.MONGODB_URI);
     console.log('Connected to MongoDB successfully');
   } catch (error) {
